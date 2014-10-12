325478cddac08dc1932391f95c022fdfd994ef24
AdySan <adityatannu@gmail.com>
2014-10-11 20:25:13 -0700

    Initial Commit

---
+++ ImageFlip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageFlip
+//
+//  Created by Aditya Tannu on 10/11/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageFlip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageFlip
+//
+//  Created by Aditya Tannu on 10/11/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
